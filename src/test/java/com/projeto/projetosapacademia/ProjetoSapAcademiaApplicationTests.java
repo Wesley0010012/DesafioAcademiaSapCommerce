51b86adf82c966d660dea619a193596ac0079c4f
Wesley0010012 <wglaurindo33@gmail.com>
2024-11-21 11:38:05 -0300

    chore: inclui estrutura inicial do projeto

---
+++ src/test/java/com/projeto/projetosapacademia/ProjetoSapAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.projetosapacademia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSapAcademiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
