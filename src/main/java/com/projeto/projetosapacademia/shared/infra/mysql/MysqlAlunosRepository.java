36c6f496b463836ad73f8266b67675360ddfadfc
Wesley0010012 <wglaurindo33@gmail.com>
2024-11-21 19:18:31 -0300

    feat: integra gateway EncontrarAlunoPorRegistroAcademico ao repositório de alunos

--- src/main/java/com/projeto/projetosapacademia/shared/infra/mysql/MysqlAlunosRepository.java
+++ src/main/java/com/projeto/projetosapacademia/shared/infra/mysql/MysqlAlunosRepository.java
@@ -4,10 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.projeto.projetosapacademia.shared.domain.entities.Aluno;
+import com.projeto.projetosapacademia.shared.domain.gateways.EncontrarAlunoPorRegistroAcademico;
 import com.projeto.projetosapacademia.shared.domain.gateways.RegistrarAluno;
 import com.projeto.projetosapacademia.shared.domain.gateways.VerificarAlunoPorEmail;
 
 @Repository()
-public interface MysqlAlunosRepository extends JpaRepository<Aluno, Long>, RegistrarAluno, VerificarAlunoPorEmail {
+public interface MysqlAlunosRepository extends JpaRepository<Aluno, Long>, RegistrarAluno, VerificarAlunoPorEmail, EncontrarAlunoPorRegistroAcademico {
 
 }
