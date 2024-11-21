ba51a4b24191446a18a9d3ededaecc49c5875d54
Wesley0010012 <wglaurindo33@gmail.com>
2024-11-21 13:30:15 -0300

    feat: implementa repositório MysqlAlunosRepository

---
+++ src/main/java/com/projeto/projetosapacademia/shared/infra/mysql/MysqlAlunosRepository.java
@@ -0,0 +1,13 @@
+package com.projeto.projetosapacademia.shared.infra.mysql;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projeto.projetosapacademia.shared.domain.entities.Aluno;
+import com.projeto.projetosapacademia.shared.domain.gateways.RegistrarAluno;
+import com.projeto.projetosapacademia.shared.domain.gateways.VerificarAlunoPorEmail;
+
+@Repository()
+public interface MysqlAlunosRepository extends JpaRepository<Aluno, Long>, RegistrarAluno, VerificarAlunoPorEmail {
+
+}
